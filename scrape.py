8c91a7883b6ff67dbead37dc84b470b89b8b584b
David Bradway <dpb6@duke.edu>
2021-05-19 14:23:20 -0400

    add number of streets to message

--- scrape.py
+++ scrape.py
@@ -15,17 +15,23 @@ def main():
     else:
         old_string = ''
 
-    page = requests.get(config.url)
+    page = requests.get(config.url, timeout=5)
     soup = BeautifulSoup(page.content, 'html.parser')
+    page.close()
     results = soup.find(class_=config.target_class)
     date_string = results.get_text()
+    ul = soup.find('ul')  # Start here
+    lis = []
+    for li in ul.findAll('li'):
+        lis.append(li)
+    result2 = lis[1].find('a')
+    streets = result2.get_text()
 
+    print(streets + date_string)
     if date_string != old_string:
-        print('New Streets!', date_string)
-
         if config.api_key != 'replaceme':
             pb = Pushbullet(config.api_key)
-            pb.push_note('New Streets!', date_string)
+            pb.push_note(streets, date_string)
 
         with open(cache_file, 'wb') as cache_handle:
             print("saving result to cache '%s'" % cache_file)
